7badba933b983b2788b23d2e7a43ef0fc2090bee
Alexander Tauenis <atauenis@yandex.ru>
2020-05-21 23:26:51 +0300

    replace ftp client

--- FTPC.cs
+++ FTPC.cs
@@ -9,59 +9,131 @@ using System.Threading.Tasks;
 
 namespace WebOne
 {
-    /// <summary>
-    /// FTP control stream operation client
-    /// </summary>
-    public class FtpOperation
-    {
-        LogWriter Log;
-        TcpClient Client;
-        NetworkStream ClientStream;
+	/// <summary>
+	/// FTP control stream operation client
+	/// </summary>
+	public class FtpOperation : IDisposable
+	{
+		//https://stackoverflow.com/questions/2321097/how-to-send-arbitrary-ftp-commands-in-c-sharp
+		LogWriter Log;
+		TcpClient tcpClient = new TcpClient();
 
-        public FtpOperation(string ServerName, LogWriter Log, Action<string> ResponseHandler, Stream userClientStream)
-        {
-            this.Log = Log;
-            Log.WriteLine("Connecting: {0}", ServerName);
+		public FtpOperation(LogWriter Log)
+		{
+			this.Log = Log;
+		}
 
-            Client = new TcpClient(ServerName, 21);
-            ClientStream = Client.GetStream();
+		/// <summary>
+		/// Connect to a FTP server
+		/// </summary>
+		/// <param name="serverName">Destination host name</param>
+		/// <param name="port">Destination port</param>
+		/// <returns>Welcome message of the server</returns>
+		public string Connect(string serverName, int port)
+		{
+			Log.WriteLine("FTPC: connect to {0}", serverName);
+			string resp;
+			tcpClient.Connect(serverName, port);
+			Log.WriteLine("FTPC: connected to {0}", serverName);
+			resp = ReceiveResponse(tcpClient.GetStream());
+			Flush(tcpClient);
+			Log.WriteLine("FTPC: successful connect to {0}", serverName);
+			return resp;
+		}
 
-            //SendString("USER " + Username + "\n" + "PASS" + Password + "\n", ResponseHandler);
-            //SendString("USER anonymous\r\nPASS test\r\n", ResponseHandler);
+		/// <summary>
+		/// Send a FTP command to server
+		/// </summary>
+		/// <param name="command">The command</param>
+		/// <returns>Server's response</returns>
+		public string SendFtpCommand(string command)
+		{
+			return TransmitCommand(tcpClient, command);
+		}
 
-            /*"200 WebOne: Connection to " + ServerName + " established.\r\n"*/
-            ReadResponse(ResponseHandler,  userClientStream);
-        }
+		/// <summary>
+		/// Transmit a FTP command to server
+		/// </summary>
+		/// <param name="tcpClient">Link with the server</param>
+		/// <param name="cmd">The command (without line feed on the end)</param>
+		/// <returns>Server's response</returns>
+		private string TransmitCommand(TcpClient tcpClient, string cmd)
+		{
+			var networkStream = tcpClient.GetStream();
+			if (!networkStream.CanWrite || !networkStream.CanRead)
+				return string.Empty;
 
-        public void ReadResponse(Action<string> ResponseHandler,  Stream userClientStream = null)
-        {
-            new Task(() =>
-            {
-                if (userClientStream != null)
-                {
-                    try
-                    {
-                        ClientStream.CopyTo(userClientStream);
-                    }
-                    catch (IOException)
-                    {
-                        Log.WriteLine("FTP <!<!<! Connection to the server lost.");
-                    }
-                }
-            }).Start();
-            Thread.Sleep(1000);
-        }
+			var sendBytes = Encoding.ASCII.GetBytes(cmd + "\r\n");
+			networkStream.Write(sendBytes, 0, sendBytes.Length);
+			
+			return ReceiveResponse(networkStream);
+		}
 
-        /// <summary>
-        /// Send a string to remote server.
-        /// </summary>
-        /// <param name="Request">The string.</param>
-        /// <param name="ResponseHandler">Handler for server response.</param>
-        public void SendString(string Request, Action<string> ResponseHandler, Stream userClientStream)
-        {
-            byte[] data = Encoding.ASCII.GetBytes(Request);
-            ClientStream.Write(data, 0, data.Length);
-            ReadResponse(ResponseHandler, userClientStream);
-        }
-    }
+		/// <summary>
+		/// Receive a line from response buffer
+		/// </summary>
+		/// <param name="networkStream"></param>
+		/// <returns></returns>
+		private string ReceiveResponseLine(NetworkStream networkStream)
+		{
+			if (!networkStream.CanWrite || !networkStream.CanRead)
+				return string.Empty;
+
+			var streamReader = new StreamReader(networkStream);
+			string resp = streamReader.ReadLine() + "\r\n";
+			if (resp == "\r\n") resp = string.Empty;
+			Flush(tcpClient);
+			return resp;
+		}
+
+		/// <summary>
+		/// Get last full response from the remote server
+		/// </summary>
+		/// <param name="networkStream">Stream from the server</param>
+		/// <returns>The server's response</returns>
+		public string ReceiveResponse(NetworkStream networkStream)
+		{
+			string resp = string.Empty;
+			string lastResp = ReceiveResponseLine(networkStream);
+			try
+			{
+				while (lastResp != string.Empty)
+				{
+					resp += lastResp;
+					lastResp = ReceiveResponseLine(networkStream);
+				}
+			}
+			catch { /*Stop at end of the buffer*/ }
+
+			return resp;
+		}
+
+		private string Flush(TcpClient tcpClient)
+		{
+			try
+			{
+				var networkStream = tcpClient.GetStream();
+				if (!networkStream.CanWrite || !networkStream.CanRead)
+					return string.Empty;
+
+				var receiveBytes = new byte[tcpClient.ReceiveBufferSize];
+				networkStream.ReadTimeout = 500;
+				networkStream.Read(receiveBytes, 0, tcpClient.ReceiveBufferSize);
+
+				return Encoding.ASCII.GetString(receiveBytes).Trim('\0');
+			}
+			catch
+			{
+				// Ignore all irrelevant exceptions
+			}
+
+			return string.Empty;
+		}
+
+		public void Dispose()
+		{
+			if (tcpClient.Connected)
+				tcpClient.Close();
+		}
+	}
 }
--- FTPTransit.cs
+++ FTPTransit.cs
@@ -10,7 +10,7 @@ namespace WebOne
 	/// <summary>
 	/// FTP Proxy traffic transit
 	/// </summary>
-    public class FTPTransit
+	public class FTPTransit
 	{
 		public TcpClient Client;
 		NetworkStream ClientStream = null;
@@ -18,8 +18,9 @@ namespace WebOne
 		LogWriter Logger = new LogWriter();
 		FtpOperation FtpClient = null;
 
-		string UserName = "";
-		string Password = "";
+		string ServerName = "localhost";
+		string UserName = "anonymous";
+		string UserPassword = "webone@github.com";
 
 		public FTPTransit(TcpClient TcpClient)
 		{
@@ -27,24 +28,16 @@ namespace WebOne
 		}
 
 
-		//http://www.ciscolab.ru/security/page,3,36-maloizvestnye-podrobnosti-raboty-nat.html
-		/*
-		220-Welcome to Yandex Mirror FTP service. Your served by: mirror01vla.mds.yandex
-		.net
-		220
-		USER anonymous
-		331 Please specify the password.
-		PASS test
-		530 Login incorrect.
-		USER anonymous
-		331 Please specify the password.
-		PASS test
-		230 Login successful.
-		421 Timeout.
-
-
-		Подключение к узлу утеряно.
-
+		/* About FTP ALG (on Russian):
+		 * http://www.ciscolab.ru/security/page,3,36-maloizvestnye-podrobnosti-raboty-nat.html
+		 * 
+		 * Chat: (220, USER, 331, PASS, 230...421 :) )
+		 * Client: PORT 192,16,0,101,4,211
+		 * Server: 200 PORT command successful. Consider using PASV.
+		 * Client: RETR file.zip
+		 * Server: 150 Opening BINARY mode data connection for file.zip (1334109 bytes).
+		 * 
+		 * 192,16,0,101,4,211 -> IP:[(4*256) + 211 = 1235] -> 192.168.0.101:1235
 		*/
 
 		/// <summary>
@@ -58,9 +51,11 @@ namespace WebOne
 			try
 			{
 				ClientStream = Client.GetStream();
-				ReturnString("220 WebOne FTP Proxy Server ({0})\r\n",Environment.OSVersion.Platform);
-				ReturnString("220 Please enter remote server name:\r\n");
-				//ReturnString("220 ftp.example.com anonymous\n");
+				string WelcomeMessage = 
+					"220 WebOne FTP Proxy Server.\r\n" +
+					"220 Please enter  remote server name  or  user and server name:\r\n" +
+					"220     ftp.example.com     or     user@ftp.example.com\r\n";
+				ReturnString(WelcomeMessage);
 
 				byte[] DataBuffer = new byte[64];
 				while (true)
@@ -79,38 +74,62 @@ namespace WebOne
 					if (Request == "") { DataBuffer = new byte[64]; continue; }
 					Logger.WriteLine("FTP {0}: {1}.", ClientIP, Request);
 
+					//extract command name and arguments
 					string CmdName = Request;
 					string CmdArgs = "";
 					if (Request.Contains(" ")) CmdName = Request.Substring(0, Request.IndexOf(" "));
+					if (CmdName.Contains("\r\n")) CmdName = CmdName.Substring(0, CmdName.Length - 2);
 					if (Request.Contains(" ")) CmdArgs = Request.Substring(Request.IndexOf(" ") + 1);
+					if (Request.Contains(" ")) CmdArgs = CmdArgs.Substring(0, CmdArgs.Length - 2);
+
+					//parse commands
 					switch (CmdName)
 					{
 						case "USER":
 							if (FtpClient == null)
 							{
-								UserName = CmdArgs.Substring(0, CmdArgs.Length - 2);
-								ReturnString("331 WebOne: Please specify the password (any).\r\n");
+								if(CmdArgs.ToLower() == "anonymous" || CmdArgs.Length < 1)
+								{
+									ReturnString("430 WebOne: Please use this syntax: USER username@ftp.example.com!\r\n");
+									DataBuffer = new byte[64];
+									continue;
+
+								}
+								if (CmdArgs.Contains("@"))
+								{
+									int delimeter = CmdArgs.IndexOf("@");
+									ServerName = CmdArgs.Substring(delimeter + 1);
+									UserName = CmdArgs.Substring(0, delimeter);
+								}
+								else ServerName = CmdArgs;
+								ReturnString("331 WebOne: Please specify the password for '{0}' on '{1}'.\r\n", UserName, ServerName);
 							}
 							else
-								FtpClient.SendString(Request, ProcessResponse, ClientStream);
+								ReturnString(FtpClient.SendFtpCommand(CmdName + " " + CmdArgs));
 							DataBuffer = new byte[64];
 							continue;
 						case "PASS":
-							Password = CmdArgs.Substring(0, CmdArgs.Length - 2);
+							UserPassword = CmdArgs.Substring(0, CmdArgs.Length - 2);
 							if (FtpClient == null)
 							{
-								FtpClient = new FtpOperation(UserName, Logger, ProcessResponse, ClientStream);
+								UserPassword = CmdArgs;
+								FtpClient = new FtpOperation(Logger);
+								string str = "211 WebOne: Connection successful.\r\n" + FtpClient.Connect(ServerName, 21) + "211 WebOne: Will use {0}/{1} account.\r\n";
+								str += FtpClient.SendFtpCommand("USER " + UserName);
+								str += FtpClient.SendFtpCommand("PASS " + UserPassword);
+								ReturnString(str,UserName,UserPassword);
 							}
 							else
-								FtpClient.SendString(Request, ProcessResponse, ClientStream);
+								ReturnString(FtpClient.SendFtpCommand(CmdName + " " + CmdArgs));
 							DataBuffer = new byte[64];
 							continue;
 						case "PORT":
 							ReturnString("425 WebOne: Active to Passive converting is not currently implemented.\r\n");
 							continue;
 						default:
-							if(FtpClient!=null)
-							FtpClient.SendString(Request, ProcessResponse, ClientStream);
+							if (FtpClient != null)
+								ReturnString(FtpClient.SendFtpCommand(CmdName + " " + CmdArgs));
+							else ReturnString("530 WebOne: No uplink connection.\r\n");
 							DataBuffer = new byte[64];
 							continue;
 					}
@@ -123,6 +142,13 @@ namespace WebOne
 			catch (Exception ex)
 			{
 				Logger.WriteLine("FTP {0}: Error: {1}.", ClientIP, ex.Message);
+
+				try
+				{
+					if (ClientStream != null)
+					{ ReturnString("421 WebOne: An error occured: {0}.\r\n", ex.Message); }
+				}
+				catch { }
 			}
 			finally
 			{
@@ -142,17 +168,7 @@ namespace WebOne
 		{
 			byte[] data = Encoding.ASCII.GetBytes(string.Format(Response, Args));
 			ClientStream.Write(data, 0, data.Length);
-		}
-
-		/// <summary>
-		/// Process remote server's response.
-		/// </summary>
-		/// <param name="Response">The response from control stream.</param>
-		private void ProcessResponse(string Response)
-		{
-			if (Response == "") return;
-			Logger.WriteLine("FTP {0}: {1}.", "  < < <  ", Response);
-			ReturnString("{0}\r\n", Response);
+			ClientStream.Flush();
 		}
 	}
 }
